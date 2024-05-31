648a3e29efe4ce523ad1c4bc0b7cdde29aece877
rin00000 <rinjun@naver.com>
2024-05-31 13:57:57 +0900

    Feat: layout 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import Layout from "./components/layout/layout";
+import Detail from "./pages/Detail";
 import Home from "./pages/Home";
+
 function App() {
-  return <Home />;
+  return (
+    <Layout>
+      <Home /> // children
+    </Layout>
+  );
 }
 
 export default App;
